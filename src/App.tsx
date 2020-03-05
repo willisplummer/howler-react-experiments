1b85f555f63bf2e438c1acd7050e8007193bfa15
willis plummer <willisplummer@gmail.com>
2020-03-05 00:33:37 -0500

    working stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { Howl } from 'howler';
+import Slider from '@material-ui/core/Slider'
 import './App.css';
 
 function App() {
+  const [song] = useState(new Howl({ 
+      src: `${process.env.PUBLIC_URL}/SoundHelix-Song-1.mp3`,
+      loop: true,
+      volume: 0.5,
+      preload: true,
+      rate: 1.0,
+      onloaderror: console.error,
+  }))
+
+  useEffect(() => {
+    // Update the document title using the browser API
+    // rate can be between 0 and 4
+    const convertedSliderValue = (sliderValue / 100) * 4
+
+    song.rate(convertedSliderValue)
+  });
+
+  const playSound = () => {
+    song.play()
+  }
+  const pauseSound = () => {
+    song.pause()
+  }
+
+  const [sliderValue, setSliderValue] = useState(25);
+
+  const handleSliderChange = (e: any, v: number | number[]) => {
+    const newValue = typeof v === 'number' ? v : 0
+    setSliderValue(newValue)
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={playSound}>Play</button>
+        <button onClick={pauseSound}>Pause</button>
+        <Slider value={sliderValue} onChange={handleSliderChange} aria-labelledby="continuous-slider" />
       </header>
     </div>
   );
